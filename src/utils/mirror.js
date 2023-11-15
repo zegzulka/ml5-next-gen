e31f4bd752feaaccedc113a230525cd191a3d495
Ziyuan Lin <zl4140@nyu.edu>
2023-11-15 14:33:09 -0500

    update mirror function to handle only videos

--- src/utils/mirror.js
+++ src/utils/mirror.js
@@ -29,25 +29,20 @@ function mirrorVideo(video) {
 /**
  * Takes in a HTML or p5 video and flip it horizontally.
  * The function return new instance of media and leaves the original media unmodified.
- * @param media - a HTML or p5 image or video to do the mirroring on.
+ * @param video - a HTML or p5 video to do the mirroring on.
  * @return a new mirror HTML or p5 image or video, matching the type of the media parameter.
  */
-export default function mirror(media) {
-  let isP5Media = media.elt !== undefined;
-  let rawMedia = isP5Media ? media.elt : media;
-  let mirroredMedia;
+export default function mirror(video) {
+  let isP5Element = video.elt !== undefined;
+  let mirroredVideo;
 
-  typeof HTMLVideoElement !== "undefined" && media instanceof HTMLVideoElement;
-  if (rawMedia instanceof HTMLImageElement) {
-    mirroredMedia = mirrorImage(rawMedia);
-  } else if (rawMedia instanceof HTMLVideoElement) {
+  if (rawMedia instanceof HTMLVideoElement) {
     mirroredMedia = mirrorVideo(rawMedia);
-  } else if (rawMedia instanceof HTMLCanvasElement) {
-    mirroredMedia = mirrorCanvas(rawMedia);
+  } else if (isP5Element && rawMedia.elt instanceof HTMLVideoElement) {
   } else {
     throw new Error(
-      "The media parameter passed into the mirror() function is not a valid HTML or p5 image or video."
+      "The media parameter passed into the mirror() function is not a valid HTML or p5 video."
     );
   }
-  return mirroredMedia;
+  return mirroredVideo;
 }
