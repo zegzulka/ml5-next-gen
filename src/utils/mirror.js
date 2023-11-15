9d77d4b2f0df123b7d060e466f199c307c68e46c
Ziyuan Lin <zl4140@nyu.edu>
2023-11-15 11:29:28 -0500

    add mirror video function

--- src/utils/mirror.js
+++ src/utils/mirror.js
@@ -1,13 +1,40 @@
 /**
- * Takes in a HTML or p5 image or video and flip it horizontally.
+ * Takes in a HTML video and return a new HTML video that is mirrored horizontally.
+ * @param video - a HTML video to do the mirroring on.
+ * @return a new mirror HTML video.
+ */
+function mirrorVideo(video) {
+  let canvas = document.createElement("canvas");
+  canvas.width = video.width;
+  canvas.height = video.height;
+  let ctx = canvas.getContext("2d");
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
+
+  const drawFrame = () => {
+    if (!video || !canvas) return;
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+    requestAnimationFrame(drawFrame);
+  };
+  requestAnimationFrame(drawFrame);
+
+  let mirroredVideo = document.createElement("video");
+  mirroredVideo.width = video.width;
+  mirroredVideo.height = video.height;
+  mirroredVideo.srcObject = canvas.captureStream();
+  mirroredVideo.muted = true; // for chrome autoplay policy
+  return mirroredVideo;
+}
+
+/**
+ * Takes in a HTML or p5 video and flip it horizontally.
  * The function return new instance of media and leaves the original media unmodified.
  * @param media - a HTML or p5 image or video to do the mirroring on.
  * @return a new mirror HTML or p5 image or video, matching the type of the media parameter.
  */
 export default function mirror(media) {
-  let isP5Media = media.elt;
+  let isP5Media = media.elt !== undefined;
   let rawMedia = isP5Media ? media.elt : media;
-
   let mirroredMedia;
 
   typeof HTMLVideoElement !== "undefined" && media instanceof HTMLVideoElement;
@@ -22,4 +49,5 @@ export default function mirror(media) {
       "The media parameter passed into the mirror() function is not a valid HTML or p5 image or video."
     );
   }
+  return mirroredMedia;
 }
