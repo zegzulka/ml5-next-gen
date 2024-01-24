bd115b8be982b912b3b2332fc2329b497089e93c
Jakub Zegzulka <jakub@zegzulka.cz>
2024-01-24 13:25:40 -0800

    working w/o transparency - jsgif

--- examples/BodySegmentation-mask-stickers/sketch.js
+++ examples/BodySegmentation-mask-stickers/sketch.js
@@ -1,11 +1,8 @@
 let bodyPix;
 let video;
 let segmentation;
-let isVideoPlaying = true;
-let gif;
-let captureDuration = 2; // Duration in seconds
 let frameRate = 1; // Frames per second
-let quality = 10; // GIF quality
+let captureDuration = 2; // Duration in seconds
 
 function preload() {
   bodyPix = ml5.bodySegmentation("SelfieSegmentation", {
@@ -25,10 +22,8 @@ function setup() {
     .addEventListener("click", startGIFCapture);
 }
 
-loadGIFWorker();
-
 function draw() {
-  clear(); // Use clear() instead of background()
+  clear();
   if (segmentation) {
     video.mask(segmentation);
     image(video, 0, 0);
@@ -39,58 +34,74 @@ function gotResults(result) {
   segmentation = result.mask;
 }
 
-function toggleVideo() {
-  // Toggle the video playback
-  if (isVideoPlaying) {
-    video.pause();
-    isVideoPlaying = false;
-  } else {
-    video.loop();
-    isVideoPlaying = true;
-  }
-}
-
-function updateGIFSettings() {
-  frameRate = parseInt(document.getElementById("frameRate").value);
-  quality = parseInt(document.getElementById("quality").value);
-  captureDuration = parseInt(document.getElementById("duration").value);
-  const loopCount = parseInt(document.getElementById("loopCount").value);
-  const frameDelay = parseInt(document.getElementById("frameDelay").value);
-
-  gif.options.quality = quality;
-  gif.options.repeat = loopCount;
-
-  return frameDelay;
-}
-
 function startGIFCapture() {
-  let frameCount = captureDuration * frameRate;
-  for (let i = 0; i < frameCount; i++) {
-    setTimeout(() => {
-      clear();
-      gif.addFrame(video.canvas, { delay: 1000 / frameRate, copy: true });
-      if (i === frameCount - 1) {
-        gif.render();
-      }
-    }, (i * 1000) / frameRate);
+  console.log("Button clicked"); // Add this line for debugging
+  let frames = [];
+  let captureFrameCount = captureDuration * frameRate;
+  let interval = 1000 / frameRate;
+
+  function captureFrame() {
+    if (frames.length < captureFrameCount) {
+      frames.push(
+        document.getElementById("defaultCanvas0").toDataURL("image/jpeg")
+      );
+      setTimeout(captureFrame, interval);
+    } else {
+      createGIFfromFrames(frames);
+    }
   }
+
+  captureFrame();
 }
 
-function loadGIFWorker() {
-  fetch("https://cdnjs.cloudflare.com/ajax/libs/gif.js/0.2.0/gif.worker.js")
-    .then((response) => response.blob())
-    .then((workerBlob) => {
-      const workerBlobUrl = URL.createObjectURL(workerBlob);
+function createGIFfromFrames(frames) {
+  let encoder = new GIFEncoder();
+  encoder.setRepeat(0);
+  encoder.setDelay(200);
 
-      gif = new GIF({
-        workers: 2,
-        quality: quality,
-        workerScript: workerBlobUrl,
-      });
+  // Set the transparent color to rgba(0, 0, 0, 0) for full transparency
+  encoder.setTransparent([0, 0, 0, 0]);
 
-      gif.on("finished", function (blob) {
-        window.open(URL.createObjectURL(blob));
-      });
-    })
-    .catch(console.error);
+  encoder.start();
+
+  let processedFrames = 0;
+
+  frames.forEach(function (frameDataURL) {
+    let img = new Image();
+    img.src = frameDataURL;
+
+    img.onload = function () {
+      let canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      // Ensure that the canvas has a transparent background
+      let context = canvas.getContext("2d", { alpha: true });
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Draw the image onto the canvas with transparency
+      context.drawImage(img, 0, 0);
+
+      encoder.addFrame(context);
+
+      processedFrames++;
+
+      if (processedFrames === frames.length) {
+        encoder.finish();
+        var binary_gif = encoder.stream().getData();
+        var data_url = "data:image/gif;base64," + encode64(binary_gif);
+        console.log("Data URL:", data_url); // Log the data URL
+
+        // Create a download link and trigger the download
+        var a = document.createElement("a");
+        a.href = data_url;
+        a.download = "animated.gif";
+        a.style.display = "none";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+    };
+  });
 }
+
